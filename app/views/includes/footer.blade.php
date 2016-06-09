5d332a185a598e86b7df4aac625559d0d289461f
Sergio Alcala <checo_2k2@hotmail.com>
2016-06-09 18:19:34 -0500

    busquedas listas

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,25 +1,34 @@
 <footer class="page-footer grey darken-2">
   <div class="container">
     <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">Footer Content</h5>
-        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">Links</h5>
+      <div class="col l3 s12">
+        <h5 class="white-text">Enlaces</h5>
         <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Aviso de Privacidad</a></li>
+        </ul>
+      </div>
+      <div class="col l4 s12 center-align">
+        <h5 class="white-text">Nuestras Redes Sociales</h5>
+        <a class="waves-effect waves-dark btn-flat white"><i class="zmdi zmdi-facebook"></i></a>
+        <a class="waves-effect waves-dark btn-flat white"><i class="zmdi zmdi-twitter"></i></a>
+        <a class="waves-effect waves-dark btn-flat white"><i class="zmdi zmdi-google-plus"></i></a>
+      </div>
+
+      <div class="col l4 offset-l1 s12">
+        <h5 class="white-text">Contacto</h5>
+        <p class="grey-text text-lighten-4">Eje Central Lazaro Cardenas 54 Local 303 Segundo Piso, Ciudad de México</p>
+        <ul>
+          <li><a class="grey-text text-lighten-3" href="#!">Telefono: 55-18-17-47</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Email: info@daseda.net</a></li>
+          <li><a class="grey-text text-lighten-3" href="#!">Whats App: 55-18-00-42-82 / 55-32-24-34-59</a></li>
         </ul>
       </div>
     </div>
   </div>
   <div class="footer-copyright grey darken-3">
     <div class="container">
-    © 2014 Copyright Text
-    <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    © 2016 Daseda S.A. de C.V. Derechos Resevados
+    <a class="grey-text text-lighten-4 right" href="#!">Sitio desarrollado Por Sergio Alcalá</a>
     </div>
   </div>
 </footer>
